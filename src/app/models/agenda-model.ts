a364de95f575075dcc9eb235bf524807cd3ec7cd
warl0ck1111 <bashirokala@gmail.com>
2021-07-16 20:10:56 +0100

    add agenda model

---
+++ src/app/models/agenda-model.ts
@@ -0,0 +1,21 @@
+export interface Agenda {
+
+  id: string;
+
+  name: string;
+
+  title: string;
+
+  date: Date;
+
+  details: string;
+
+  isCompleted: boolean;
+
+  time: Date;
+
+  isImportant: boolean;
+
+  isDeleted: boolean;
+
+}
