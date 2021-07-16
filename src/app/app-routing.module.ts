28f44dd7cdfcf44d6b3b7d26016ff081724be866
warl0ck1111 <bashirokala@gmail.com>
2021-07-16 20:12:07 +0100

    update app-routing with navigation links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendaListComponent } from './agenda-list/agenda-list.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component:AgendaListComponent
+
+  },
+  {
+    path:"home", component:AgendaListComponent
+
+  },
+
+  {
+    path:"create", component:AgendaComponent
+
+  },
+  {
+    path:"edit", component:AgendaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
