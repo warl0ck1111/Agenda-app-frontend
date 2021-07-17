0ebb27658e30b22d5e699713d914e0970f08b41a
warl0ck1111 <bashirokala@gmail.com>
2021-07-17 16:23:05 +0100

    add import and export excel implementation as well as clean code to remove unused imports and variables

--- src/app/agenda-list/agenda-list.component.ts
+++ src/app/agenda-list/agenda-list.component.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, of, timer } from 'rxjs';
+import { timer } from 'rxjs';
+
+import { Subject } from 'rxjs';
+import * as XLSX from 'xlsx';
+
 import {
-  catchError,
-  debounceTime,
-  distinctUntilChanged,
   finalize,
-  map,
-  switchMap,
-  tap,
 } from 'rxjs/operators';
 import { Agenda } from '../models/agenda-model';
+import { ExcelService } from '../services/excel/excel.service';
+import { AjaxResponse } from '../models/ajax-response-model';
 
 @Component({
   selector: 'app-agenda-list',
@@ -21,44 +21,29 @@ import { Agenda } from '../models/agenda-model';
 })
 export class AgendaListComponent implements OnInit {
 
-  // agenda!:Agenda
-  agendaList!: Agenda[]
-  selectedId: any;
+  GET_ALL_AGENDAS_URL:string = "http://localhost:8080/api/agenda/get-all";
+  DELETE_AGENDAS_URL:string = "http://localhost:8080/api/agenda/delete/";
 
-  isLoading: boolean = true;
-  serviceTypeList: any[] = [];
-  serviceTypeForm!: FormGroup;
-  localServiceTypeList: any[] = [];
-  noRecord!: boolean;
-  isUploadLoading: boolean = false;
-  updateData: any;
-  createResponse!: any;
-  isCreateLoading: boolean = false;
-  selectedAssetId: any;
-  isStampRequired: boolean = false;
-  istaxable: boolean = false;
-  instrumentNotFound: boolean = false;
-  selectedCompany: any;
-  searching: boolean = false;
-  searchFailed: boolean = false;
-  onInstrumentSelected: any;
+  agendaList: Agenda[] = []
 
-  tablePage: number = 1;
-  tablePageSize: number = 10;
+  importAgendas: Agenda[] = this.agendaList;
+  exportAgendas: Agenda[] = [];
+
 
   alertMsg!: string;
   style!: string;
   show: boolean = false;
   interval: any;
-  icon!: string;
+  alertIcon!: string;
 
   constructor(
     private http: HttpClient,
     private router: Router,
-    private formBuilder: FormBuilder
+    private excelService: ExcelService,
   ) {
     sessionStorage.removeItem('id');
   }
+
 
   ngOnInit(): void {
     this.findAllAgendas();
@@ -66,9 +51,9 @@ export class AgendaListComponent implements OnInit {
 
 
   findAllAgendas() {
-    this.http.get("http://localhost:8080/api/agenda/get-all").subscribe((response: any) => {
-      this.agendaList = response;
-      console.log(response);
+    this.http.get(this.GET_ALL_AGENDAS_URL).subscribe((response: AjaxResponse<Agenda[]>) => {
+      this.agendaList = response.data as Agenda[];
+      // console.log(response);
 
     })
   }
@@ -86,19 +71,105 @@ export class AgendaListComponent implements OnInit {
   }
 
   deleteAgenda(agenda: Agenda) {
-    this.http.delete(`http://localhost:8080/api/agenda/delete/${agenda.id}`)
-    .pipe(finalize(()=>{
-      this.findAllAgendas();
-    }))
-    .subscribe((response: any) => {
-      this.agendaList = response;
-      console.log(response);
+    this.http.delete(`${this.DELETE_AGENDAS_URL}${agenda.id}`)
+      .pipe(finalize(() => {
+        this.findAllAgendas();
+      }))
+      .subscribe((response: any) => {
+
+        if (response.status == "OK") {
+          this.showAlert('Agenda Deleted Successfully', "warning", "info-circle")
+
+          this.agendaList = response.data;
+
+        }
+
+        // console.log(response);
 
 
-    })
+
+      },(error:AjaxResponse<null>)=>{
+        this.showAlert(error.message as string, "warning", "warning")
+      })
   }
 
-  getStyleClass(){
+  exportData(tableId: string) {
+    this.excelService.exportToFile("contacts", tableId);
+    this.showAlert("Data Exported Succesfully", "info", "info-circle")
 
   }
+
+
+
+  spinnerEnabled = false;
+  keys?: string[] | null;
+  dataSheet: any = new Subject();
+  @ViewChild('inputFile') inputFile!: ElementRef;
+  isExcelFile!: boolean;
+
+  onChange(evt: any) {
+    this.agendaList = [];
+    let data: any[], header: any;
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    this.isExcelFile = !!target.files[0].name.match(/(.xls|.xlsx)/);
+    if (target.files.length > 1) {
+      this.inputFile.nativeElement.value = '';
+    }
+    if (this.isExcelFile) {
+      this.spinnerEnabled = true;
+      const reader: FileReader = new FileReader();
+      reader.onload = (e: any) => {
+        /* read workbook */
+        const bstr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+        /* grab first sheet */
+        const wsname: string = wb.SheetNames[0];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+        /* save data */
+        data = XLSX.utils.sheet_to_json(ws);
+
+        // console.log(data);
+
+        this.agendaList = data
+        this.agendaList.map(x=>{
+          if(typeof x.isCompleted == 'string'){
+
+            x.isCompleted = JSON.parse(<string>(x.isCompleted))
+            // console.log(x.isCompleted);
+          }
+
+
+        })
+
+      };
+
+      reader.readAsBinaryString(target.files[0]);
+
+      reader.onloadend = (e) => {
+        this.spinnerEnabled = false;
+        this.keys = Object.keys(data[0]);
+        this.dataSheet.next(data)
+      }
+    } else {
+      this.showAlert("Invalid Excel File", "warning", "info-circle")
+      this.inputFile.nativeElement.value = '';
+    }
+
+    this.showAlert("Data Successfully Imported", "info", "info-circle")
+
+  }
+
+
+  showAlert(msg: string, style: string, icon:string) {
+    this.alertIcon = icon;
+    this.alertMsg = msg;
+    this.style = style || 'info';
+    this.show = true;
+    timer(5000).subscribe(() => (this.show = false));
+    return false;
+}
+
+
 }
